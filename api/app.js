abe8179d4b847accf046bdf4c1c17939032359db
0fism <cngai.yu@gmail.com>
2024-04-19 15:19:10 +0800

    Update app.js

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ const app = express();
 // app.use(cors({origin:process.env.CLIENT_URL, credentials: true}));
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: [" https://mern-api-ruddy.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
