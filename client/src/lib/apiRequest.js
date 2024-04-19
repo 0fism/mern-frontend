b9c722ca0d35bd3bc0118334e69c30f65dea658e
0fism <cngai.yu@gmail.com>
2024-04-19 15:18:13 +0800

    update baseURL

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "http://localhost:8800/api",
+    // baseURL: "http://localhost:8800/api",
+    baseURL: "https://mern-api-ruddy.vercel.app/api",
     withCredentials: true,
 });
 
