3ec8f0d3b7d7119b821abafdae31ff9c61fbd0a2
0fism <cngai.yu@gmail.com>
2024-04-20 14:49:51 +0800

    deploy config

--- api/app.js
+++ api/app.js
@@ -11,7 +11,14 @@ import envRoute from "./routes/env.route.js";
 
 const app = express();
 
-app.use(cors({origin:process.env.CLIENT_URL, credentials: true}));
+app.use(cors(
+    {
+        origin: process.env.CLIENT_URL,
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+
 app.use(express.json())
 app.use(cookieParser())
 
