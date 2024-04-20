d4a4deffde3925dfa067b715c972dcdb85df5537
0fism <cngai.yu@gmail.com>
2024-04-20 18:39:35 +0800

    sync client images

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
     baseURL: "https://mern-backend-uyrq.onrender.com/api",
+    // baseURL: "http://localhost:8080/api",
     withCredentials: true,
 });
 
