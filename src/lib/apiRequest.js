5cd1865bbf7f8f962563a116214135f41a0a7ca4
0fism <cngai.yu@gmail.com>
2024-08-13 22:55:02 +0800

    request

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "https://mern-backend-uyrq.onrender.com/api",
+    // baseURL: "https://mern-backend-uyrq.onrender.com/api",
+    baseURL: "http://13.215.173.134:8080/api/",
     // baseURL: "http://localhost:8080/api",
     withCredentials: true,
 });
