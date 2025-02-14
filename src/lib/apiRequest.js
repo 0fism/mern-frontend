ebd17df4980dff2333a62f05dd4709e4f4d40f55
0fism <cngai.yu@gmail.com>
2025-02-14 15:45:03 +0800

    change backend api

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
     // baseURL: "https://mern-backend-uyrq.onrender.com/api",
-    baseURL: "https://estate.news24u.com/api",
+    baseURL: "https://estate.postsly.com/api",
     // baseURL: "http://localhost:8080/api",
     withCredentials: true,
 });
